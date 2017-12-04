b7e2dc42537b860b04eb504a5a2681280fb5ea3f
SelinaDeepKaur <sekaur@cs.stonybrook.edu>
2017-12-04 03:06:10 -0800

    commit two

--- pktInject.py
+++ pktInject.py
@@ -1,12 +1,18 @@
 from optparse import OptionParser
 
 if __name__ == '__main__':
+	expression = None
 	parser = OptionParser()
-	
-	parser.add_option("-i", dest="interface",default="eth0")
-	parser.add_option("-h", dest="hostsfile")
+	parser.set_conflict_handler("resolve")
+	parser.add_option('-i', dest="interface",default="eth0")
+	parser.add_option('-h', dest="hostsfile")
 
-	(options, args) = parser.parse_args()
+	(options, remainder) = parser.parse_args()
+	expression = remainder
+	print options.interface
+	print options.hostsfile
+	if expression:
+		print expression[0]
 
 
 
