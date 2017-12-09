c48e3edfb804c0a25533deaaa2cc6cbcb68d6895
SelinaDeepKaur <sekaur@cs.stonybrook.edu>
2017-12-08 18:11:14 -0800

    Enhanced detection code

--- pktDetect.py
+++ pktDetect.py
@@ -3,23 +3,59 @@ import netifaces as ni
 import os
 import socket
 import pcap
+#mport sys
+#sys.path.insert(0, 'path/to/your/py_file')
+
+#import py_file
+import scapy
 from scapy.all import *
 
 def detector(pkt):
-	if pkt.haslayer(IP) and pkt.haslayer(DNS) and pkt.haslayer(DNSRR):
-		if pkt[DNS].id in captured:
+	if pkt.haslayer(IP) and pkt.haslayer(DNS) and pkt[DNS].qr==1:
+		print pkt[DNS].qd.qname
+		#print pkt[DNSRR].rdata
+		if pkt[DNS].id in captured and (pkt[DNS].qd.qname.rstrip('.') == captured[pkt[DNS].id][DNS].qd.qname.rstrip('.')):
+			l1=[]
+			l2=[]
 			oldpkt=captured[pkt[DNS].id]
-		        if oldpkt[DNSRR].rdata != pkt[DNSRR].rdata:
-				print "DNS poisoning attempt"
+			for i in range(pkt['DNS'].ancount):
+				dnsrr=pkt['DNS'].an[i]
+				l1.append(dnsrr.rdata)
+			print l1
+			for i in range(oldpkt['DNS'].ancount):
+				dnsrr=oldpkt['DNS'].an[i]
+				l2.append(dnsrr.rdata)
+			print l2
+			l1=sorted(l1)
+			l2=sorted(l2)
+			if l1!=l2:	
+		        #if oldpkt[DNSRR].rdata != pkt[DNSRR].rdata:
+				print "DNS poisoning attempt---------------------------------------"
 				print "TXID %s Request %s"%( pkt[DNS].id, pkt[DNS].qd.qname.rstrip('.'))
-				print "Answer1 [%s]"%oldpkt[DNSRR].rdata
-				print "Answer2 [%s]"%pkt[DNSRR].rdata
+				#print "Answer1 [%s]"%oldpkt[DNSRR].rdata
+				print "Answer1"
+				print l1
+				print "Answer2"
+				print l2
+
+				#if isinstance(oldpkt[DNSRR].rdata, str):
+				#	print type(oldpkt[DNSRR].rdata)
+				#	print "Answer1 [%s]"%oldpkt[DNSRR].rdata
+				#else:
+				#	print "type--------------------------"
+				#	print type(oldpkt[DNSRR].rdata)
+				#	print "Answer1 [%s]"%oldpkt[DNSRR].rdata.decode("utf-8")
+				#if isinstance(pkt[DNSRR].rdata, str):
+				#	print "Answer2 [%s]"%pkt[DNSRR].rdata
+				#else:
+				#	print "Answer2 [%s]"%pkt[DNSRR].rdata.decode("utf-8")
 		else:
+		
 		   captured[pkt[DNS].id]=pkt
 			
 
 if __name__ == '__main__':
-	spoofedPacketsFile = None
+	#spoofedPacketsFile = ""
 	captured={}
 	parser = OptionParser()
 	parser.set_conflict_handler("resolve")
@@ -34,7 +70,14 @@ if __name__ == '__main__':
 	else:
 		exp = ""
 	
-	if spoofedPacketsFile == None:
-		sniff(filter=exp, prn=detector, store=0, iface=options.interface)
+	if options.spoofedPacketsFile!=None:
+		print "hi"
+		print options.spoofedPacketsFile
+		sniff(filter=exp, prn=detector, store=0, offline = options.spoofedPacketsFile)
+		
+		
 	else:
-		sniff(filter=exp, prn=detector, store=0, offline = spoofedPacketsFile)
+		print "there"
+		print options.spoofedPacketsFile
+		sniff(filter=exp, prn=detector, store=0, iface=options.interface)
+		
